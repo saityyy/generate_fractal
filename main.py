d71e80ac985b054ea0f90d5b70c76d8aed040a36
saityyy <sec200091@gmail.com>
2021-12-18 09:54:31 +0900

    a

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ Created on Sat Dec 09 23:56:16 2017
 """
 
 import os
-import shutil
 import cv2
 import numpy as np
 from ifs_simple import ifs_function
 from functions import *
 
 
-iteration_num = 500  # 画像生成の試行回数(各バリエーションごとの回数)
+category_num = 10  # 生成する画像の枚数(各バリエーションごとの回数)
 numof_point = 100000  # IFSのイテレーション数
-save_dir = "."
+save_dir = "./data"
 image_size = 512
-remake_dataset_flag = True  # True時、既存のデータセットが削除される
 
 
 def cal_pix(gray):
@@ -50,35 +48,24 @@ func_collection = [[linear, 0.2],
 
 if __name__ == "__main__":
     np.random.seed(1)
-    pixels_csv_dir = os.path.join(save_dir, "pixels_csv")
     img_root_dir = os.path.join(save_dir, "fractal_image")
-    if os.path.exists(pixels_csv_dir) == False:
-        os.makedirs(pixels_csv_dir)
+    cat_root_dir = os.path.join(save_dir, "parameter")
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
         os.makedirs(img_root_dir)
-    elif remake_dataset_flag:
-        # フォルダの作り直し
-        shutil.rmtree(pixels_csv_dir)
-        shutil.rmtree(img_root_dir)
-        os.makedirs(pixels_csv_dir)
-        os.makedirs(img_root_dir)
+        os.makedirs(cat_root_dir)
     else:
-        print("fractal data exits")
+        print("fractal data exists")
         exit()
-
     for i, x in enumerate(func_collection):
         func, threshold = tuple(x)
+        #threshold = 0
         class_num = 0
         img_dir = os.path.join(img_root_dir, func.__name__)
-        if not os.path.exists(img_dir):
-            os.makedirs(img_dir)
-        elif remake_dataset_flag:
-            shutil.rmtree(img_dir)
-            os.makedirs(img_dir)
-        else:
-            print("fractal data exists")
-            exit()
-        pixels_list = []
-        while(class_num < iteration_num):  # class
+        cat_dir = os.path.join(cat_root_dir, func.__name__)
+        os.makedirs(img_dir)
+        os.makedirs(cat_dir)
+        while(class_num < category_num):  # class
             a, b, c, d, e, f, prob = 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
             param_size = np.random.randint(2, 8)
             params = np.zeros((param_size, 7), dtype=float)
@@ -95,9 +82,8 @@ if __name__ == "__main__":
             class_str = '%05d' % class_num
             fractal_img = generator(params, func)
             pixels = cal_pix(fractal_img[:, :, 0].copy())
-            pixels_list.append(pixels)
             if pixels >= threshold:
                 print('save: '+func.__name__+class_str)
                 cv2.imwrite(os.path.join(img_dir, class_str + '.png'), fractal_img)
-            class_num += 1
-        np.savetxt(os.path.join(pixels_csv_dir, func.__name__+'.csv'), pixels_list, delimiter=',')
+                np.savetxt(os.path.join(cat_dir, class_str + '.csv'), params, delimiter=',')
+                class_num += 1
