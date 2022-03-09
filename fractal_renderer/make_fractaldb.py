5c92339fadbc3b7ee2dbeb716e1bd3d2066d4a19
saityyy <62131533+saityyy@users.noreply.github.com>
2022-03-09 11:01:18 +0900

    forceオプションの追加

--- fractal_renderer/make_fractaldb.py
+++ fractal_renderer/make_fractaldb.py
@@ -37,16 +37,27 @@ from ifs_function import ifs_function
 
 def conf():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--load_root', default='./data/csv', type=str, help='load csv root')
-    parser.add_argument('--save_root', default='./data/FractalDB', type=str, help='save png root')
-    parser.add_argument('--image_size_x', default=224, type=int, help='image size x')
-    parser.add_argument('--image_size_y', default=224, type=int, help='image size y')
-    parser.add_argument('--pad_size_x', default=6, type=int, help='padding size x')
-    parser.add_argument('--pad_size_y', default=6, type=int, help='padding size y')
-    parser.add_argument('--iteration', default=100000, type=int, help='iteration')
-    parser.add_argument('--draw_type', default='patch_gray', type=str, help='{point, patch}_{gray, color}_{point_gray_filter}')
-    parser.add_argument('--weight_csv', default='./fractal_renderer/weights/weights_0.2.csv', type=str, help='weight parameter')
-    parser.add_argument('--instance', default=10, type=int, help='#instance, 10 => 1000 instance, 100 => 10,000 instance per category')
+    parser.add_argument('--load_root', default='./data/csv',
+                        type=str, help='load csv root')
+    parser.add_argument(
+        '--save_root', default='./data/FractalDB', type=str, help='save png root')
+    parser.add_argument('--image_size_x', default=224,
+                        type=int, help='image size x')
+    parser.add_argument('--image_size_y', default=224,
+                        type=int, help='image size y')
+    parser.add_argument('--pad_size_x', default=6,
+                        type=int, help='padding size x')
+    parser.add_argument('--pad_size_y', default=6,
+                        type=int, help='padding size y')
+    parser.add_argument('--iteration', default=100000,
+                        type=int, help='iteration')
+    parser.add_argument('--draw_type', default='patch_gray', type=str,
+                        help='{point, patch}_{gray, color}_{point_gray_filter}')
+    parser.add_argument(
+        '--weight_csv', default='./fractal_renderer/weights/weights_0.2.csv', type=str, help='weight parameter')
+    parser.add_argument('--instance', default=10, type=int,
+                        help='#instance, 10 => 1000 instance, 100 => 10,000 instance per category')
+    parser.add_argument('-f', '--force', action="store_true")
     args = parser.parse_args()
     return args
 
@@ -65,6 +76,12 @@ if __name__ == "__main__":
 
     if not os.path.exists(os.path.join(args.save_root)):
         os.mkdir(os.path.join(args.save_root))
+    else:
+        print("folder exists")
+        if args.force:
+            print("remake fractalDB")
+        else:
+            exit()
 
     for csv_name in csv_names:
         name, ext = os.path.splitext(csv_name)
@@ -84,56 +101,67 @@ if __name__ == "__main__":
                     sigma = 3.0*np.random.rand()
                     generators = ifs_function(prev_x=0.0, prev_y=0.0, save_root=args.save_root,
                                               fractal_name=name, fractal_weight_count=padded_fractal_weight)
-                    params = np.genfromtxt(os.path.join(args.load_root, csv_name), dtype=np.str, delimiter=',')
+                    params = np.genfromtxt(os.path.join(
+                        args.load_root, csv_name), dtype=np.str, delimiter=',')
                     for param in params:
                         generators.set_param(float(param[0]), float(param[1]), float(param[2]), float(param[3]), float(param[4]), float(param[5]), float(param[6]),
                                              weight_a=float(weight[0]), weight_b=float(weight[1]), weight_c=float(weight[2]), weight_d=float(weight[3]), weight_e=float(weight[4]), weight_f=float(weight[5]))
                     generators.calculate(args.iteration)
-                    generators.draw_point(args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'gray', count, sigma)
+                    generators.draw_point(args.image_size_x, args.image_size_y,
+                                          args.pad_size_x, args.pad_size_y, 'gray', count, sigma)
 
             elif args.draw_type == 'point_gray':
                 generators = ifs_function(prev_x=0.0, prev_y=0.0, save_root=args.save_root,
                                           fractal_name=name, fractal_weight_count=padded_fractal_weight)
-                params = np.genfromtxt(os.path.join(args.load_root, csv_name), dtype=np.str, delimiter=',')
+                params = np.genfromtxt(os.path.join(
+                    args.load_root, csv_name), dtype=np.str, delimiter=',')
                 for param in params:
                     generators.set_param(float(param[0]), float(param[1]), float(param[2]), float(param[3]), float(param[4]), float(param[5]), float(param[6]),
                                          weight_a=float(weight[0]), weight_b=float(weight[1]), weight_c=float(weight[2]), weight_d=float(weight[3]), weight_e=float(weight[4]), weight_f=float(weight[5]))
                 generators.calculate(args.iteration)
-                generators.draw_point(args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'gray', 0)
+                generators.draw_point(
+                    args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'gray', 0)
 
             elif args.draw_type == 'point_color':
                 generators = ifs_function(prev_x=0.0, prev_y=0.0, save_root=args.save_root,
                                           fractal_name=name, fractal_weight_count=padded_fractal_weight)
-                params = np.genfromtxt(os.path.join(args.load_root, csv_name), dtype=np.str, delimiter=',')
+                params = np.genfromtxt(os.path.join(
+                    args.load_root, csv_name), dtype=np.str, delimiter=',')
                 for param in params:
                     generators.set_param(float(param[0]), float(param[1]), float(param[2]), float(param[3]), float(param[4]), float(param[5]), float(param[6]),
                                          weight_a=float(weight[0]), weight_b=float(weight[1]), weight_c=float(weight[2]), weight_d=float(weight[3]), weight_e=float(weight[4]), weight_f=float(weight[5]))
                 generators.calculate(args.iteration)
-                generators.draw_point(args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'color', 0)
+                generators.draw_point(
+                    args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'color', 0)
 
             elif args.draw_type == 'patch_gray':
                 for count in range(args.instance):
                     generators = ifs_function(prev_x=0.0, prev_y=0.0, save_root=args.save_root,
                                               fractal_name=name, fractal_weight_count=padded_fractal_weight)
-                    params = np.genfromtxt(os.path.join(args.load_root, csv_name), dtype=np.str, delimiter=',')
+                    params = np.genfromtxt(os.path.join(
+                        args.load_root, csv_name), dtype=np.str, delimiter=',')
                     for param in params:
                         generators.set_param(float(param[0]), float(param[1]), float(param[2]), float(param[3]), float(param[4]), float(param[5]), float(param[6]),
                                              weight_a=float(weight[0]), weight_b=float(weight[1]), weight_c=float(weight[2]), weight_d=float(weight[3]), weight_e=float(weight[4]), weight_f=float(weight[5]))
                     generators.calculate(args.iteration)
-                    generators.draw_patch(args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'gray', count)
+                    generators.draw_patch(
+                        args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'gray', count)
 
             elif args.draw_type == 'patch_color':
                 for count in range(args.instance):
                     generators = ifs_function(prev_x=0.0, prev_y=0.0, save_root=args.save_root,
                                               fractal_name=name, fractal_weight_count=padded_fractal_weight)
-                    params = np.genfromtxt(os.path.join(args.load_root, csv_name), dtype=np.str, delimiter=",")
+                    params = np.genfromtxt(os.path.join(
+                        args.load_root, csv_name), dtype=np.str, delimiter=",")
                     for param in params:
                         generators.set_param(float(param[0]), float(param[1]), float(param[2]), float(param[3]), float(param[4]), float(param[5]), float(param[6]),
                                              weight_a=float(weight[0]), weight_b=float(weight[1]), weight_c=float(weight[2]), weight_d=float(weight[3]), weight_e=float(weight[4]), weight_f=float(weight[5]))
                     generators.calculate(args.iteration)
-                    generators.draw_patch(args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'color', count)
+                    generators.draw_patch(
+                        args.image_size_x, args.image_size_y, args.pad_size_x, args.pad_size_y, 'color', count)
             fractal_weight += 1
 
     endtime = time.time()
     interval = endtime - starttime
-    print("passed time = %dh %dm %ds" % (int(interval/3600), int((interval % 3600)/60), int((interval % 3600) % 60)))
+    print("passed time = %dh %dm %ds" % (int(interval/3600),
+          int((interval % 3600)/60), int((interval % 3600) % 60)))
