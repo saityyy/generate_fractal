8607bba5f3ef5167ef131057110a6512e0fb884b
saityyy <sec200091@gmail.com>
2022-03-12 03:23:30 +0000

    a

--- exe.sh
+++ exe.sh
@@ -1,7 +1,9 @@
 category_num=1
+instance_num=1
 
 python3 fractal_renderer/ifs_search.py --rate=0.1 --category=${category_num} --numof_point=100000  --save_dir='./data'
 
-python3 ./fractal_renderer/make_fractaldb.py --draw_type="point_gray" --save_root="./data/FractalDB+normal" --instance=1
-python3 ./fractal_renderer/make_fractaldb.py --draw_type="gaussian_filter" --save_root="./data/FractalDB+gaussian" --instance=1
-python3 ./fractal_renderer/make_fractaldb.py --draw_type="uniform_filter" --save_root="./data/FractalDB+uniform" --instance=1
+mkdir ./data/filter
+python3 ./fractal_renderer/make_fractaldb.py --draw_type="point_gray" --save_root="./data/filter/FractalDB+normal" --instance=${instance_num}
+python3 ./fractal_renderer/make_fractaldb.py --draw_type="gaussian_filter" --save_root="./data/filter/FractalDB+gaussian" --instance=${instance_num}
+python3 ./fractal_renderer/make_fractaldb.py --draw_type="uniform_filter" --save_root="./data/filter/FractalDB+uniform" --instance=${instance_num}
