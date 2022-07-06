b867fa14e2693fee0e1e69cd56dbe443843676e5
saityyy <sec200091@gmail.com>
2022-07-06 12:24:08 +0900

    datetime add

--- exe.sh
+++ exe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #default 1000
-category_num=200
+category_num=1000
 #default 10
-instance_num=1
+instance_num=10
 
 #python3 fractal_renderer/ifs_search.py --rate=0.1 --category=${category_num} --numof_point=100000  --save_dir='./data'
 
@@ -13,10 +13,10 @@ python3 fractal_renderer/ifs_search.py --rate=0.1 --category=${category_num} --n
 python3 fractal_renderer/ifs_search.py --rate=0.1 --category=${category_num} --numof_point=100000 --func_num=8   --save_dir='./data/function/f8'
 python3 fractal_renderer/ifs_search.py --rate=0.1 --category=${category_num} --numof_point=100000 --func_num=-1  --save_dir='./data/function/fall'
 mkdir ./data/function/DB
-python3 ./fractal_renderer/make_fractaldb.py --draw_type="point_gray" --save_root="./data/function/DB/FractalDB_funcnum1" --load_root="./data/function/f1/csv"   --instance=${instance_num}
-python3 ./fractal_renderer/make_fractaldb.py --draw_type="point_gray" --save_root="./data/function/DB/FractalDB_funcnum4" --load_root="./data/function/f4/csv"   --instance=${instance_num}
-python3 ./fractal_renderer/make_fractaldb.py --draw_type="point_gray" --save_root="./data/function/DB/FractalDB_funcnum8" --load_root="./data/function/f8/csv"   --instance=${instance_num}
-python3 ./fractal_renderer/make_fractaldb.py --draw_type="point_gray" --save_root="./data/function/DB/FractalDB_allfunc"  --load_root="./data/function/fall/csv" --instance=${instance_num}
+python3 ./fractal_renderer/make_fractaldb.py --draw_type="gaussian_filter" --save_root="./data/function/DB/FractalDB_funcnum1" --load_root="./data/function/f1/csv"   --instance=${instance_num}
+python3 ./fractal_renderer/make_fractaldb.py --draw_type="gaussian_filter" --save_root="./data/function/DB/FractalDB_funcnum4" --load_root="./data/function/f4/csv"   --instance=${instance_num}
+python3 ./fractal_renderer/make_fractaldb.py --draw_type="gaussian_filter" --save_root="./data/function/DB/FractalDB_funcnum8" --load_root="./data/function/f8/csv"   --instance=${instance_num}
+python3 ./fractal_renderer/make_fractaldb.py --draw_type="gaussian_filter" --save_root="./data/function/DB/FractalDB_allfunc"  --load_root="./data/function/fall/csv" --instance=${instance_num}
 
 #filterの違いで比較
 #mkdir ./data/filter
@@ -30,3 +30,6 @@ python3 ./fractal_renderer/make_fractaldb.py --draw_type="point_gray" --save_roo
 #mkdir ./data/size
 #python3 ./fractal_renderer/make_fractaldb.py --draw_type="gaussian_filter" --save_root="./data/size/FractalDB+224" --instance=${instance_num} --image_size_x=224 --image_size_y=224
 #python3 ./fractal_renderer/make_fractaldb.py --draw_type="gaussian_filter" --save_root="./data/size/FractalDB+512" --instance=${instance_num} --image_size_x=512 --image_size_y=512
+
+mkdir ./data/function/weight
+python3 ./train/pretrain.py --db_path="./data/function/DB" --weight_path="./data/function/weight"
--- train/pretrain.py
+++ train/pretrain.py
@@ -3,6 +3,7 @@ import shutil
 import glob
 import random
 import pickle
+import datetime
 from unicodedata import category
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -18,9 +19,12 @@ from torchvision.datasets import ImageFolder
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--db_path', type=str)
+parser.add_argument('--weight_path', type=str)
 args = parser.parse_args()
 DB_PATH = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "..", args.db_path)
+WEIGHT_PATH = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "..", args.weight_path)
 normalize = transforms.Normalize(mean=[0.2, 0.2, 0.2], std=[0.5, 0.5, 0.5])
 transform = transforms.Compose([
     transforms.ToTensor(), normalize
@@ -40,28 +44,28 @@ class Model(nn.Module):
 
 
 epoch = 90
-batch_size = 256
-model_name = "resnet18"
+batch_size = 128
+model_name = "resnet50"
 device = torch.device("cuda"if torch.cuda.is_available() else "cpu")
-for i in os.scandir("./container_data/filter"):
-    DB_PATH = i.path
-    train_dataset = ImageFolder(DB_PATH, transform)
+for i in os.scandir(DB_PATH):
+    db_path = i.path
+    train_dataset = ImageFolder(db_path, transform)
     train_dataloader = DataLoader(
         train_dataset, batch_size=batch_size, shuffle=True)
-    print(len(train_dataset))
+    print(len(os.listdir(db_path)))
     if model_name == "resnet18":
-        model = Model(models.resnet18(pretrained=False), len(os.listdir(DB_PATH))).to(device)
+        model = Model(models.resnet18(pretrained=False), len(os.listdir(db_path))).to(device)
     elif model_name == "resnet50":
-        model = Model(models.resnet50(pretrained=False), len(os.listdir(DB_PATH))).to(device)
+        model = Model(models.resnet50(pretrained=False), len(os.listdir(db_path))).to(device)
     #weight = torch.load("weight/FractalDB+gaussian_resnet18_0.25.pth")
     # model.pretrained_model.load_state_dict(weight)
     optimizer = optim.SGD(model.parameters(), lr=1e-1)
     criterion = nn.CrossEntropyLoss().to(device)
-    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30, 60], gamma=0.1)
-    acc_list = [0]
-    print(device)
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30,60], gamma=0.1)
+    acc=0
+    print(db_path)
     for it in range(epoch):
-        for x, t in tqdm(train_dataloader):
+        for x, t in train_dataloader:
             x, t = x.to(device), t.to(device)
             y = model(x)
             loss = criterion(y, t)
@@ -69,6 +73,7 @@ for i in os.scandir("./container_data/filter"):
             loss.backward()
             optimizer.step()
             correct_sum = (torch.argmax(y, dim=1) == t).sum()
-            print(correct_sum)
         scheduler.step()
-    torch.save(model.pretrained_model.cpu().state_dict(), "./weight/{}_{}_{}.pth".format(i.name, model_name, acc_list[-1]))
+        acc=correct_sum/batch_size
+        print("epoch: {} accuracy: {} time :{}".format(it,acc,datetime.datetime.now()))
+    torch.save(model.pretrained_model.cpu().state_dict(), os.path.join(WEIGHT_PATH,"{}_{}_{}.pth".format(i.name, model_name, acc)))
